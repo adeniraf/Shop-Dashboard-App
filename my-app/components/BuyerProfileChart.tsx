358a43a82ad6539b8d67aaae1f6d4d2ead9f4c73
Fiyin Adeniranye <79162576+adeniraf@users.noreply.github.com>
2023-09-17 20:19:57 +0100

    removed label in BuyerProfileChart.tsx. will fix later

--- my-app/components/BuyerProfileChart.tsx
+++ my-app/components/BuyerProfileChart.tsx
@@ -51,7 +51,7 @@ const BuyerProfileChart = () => {
 							cx='50%'
 							cy='50%'
 							labelLine={false}
-							label={renderCustomizedLabel}
+							// label={renderCustomizedLabel}
 							outerRadius={80}
 							fill='#8884d8'
 							dataKey='value'>
